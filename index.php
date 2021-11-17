5f5a9f7a66eb5450c695825265b9706438c82b4e
Nouman Rahman <techcode786@gmail.com>
2021-11-17 13:06:48 +0530

    fixed sql

--- index.php
+++ index.php
@@ -73,16 +73,9 @@ echo "&lt;h1&gt;Hello $name&lt;/h1&gt;";
 					$this->open('database.db');
 				}
 			}
+			$sql = "";
 			$db = new Database();
 			$exec = $db->exec($sql);
-			$sql = "";
-			$result = $db->query($sql);
-			while ($row = $result->fetchArray()) {
-				echo "ID: " . $row[0] . "<br>";
-				echo "Name: " . $row[1] . "<br>";
-				echo "Age: " . $row[2] . "<br>";
-				echo "<br>";
-			}
 			if (!$db) {
 				echo $db->lastErrorMsg();
 			} else {
